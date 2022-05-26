a39dec1a55327800da9dabae49f5f79d457e6cae
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-26 16:37:03 +0530

    [Sunny] Add : Ability To Create A BST For Insert The values

---
+++ BinarySearchTree/BinaryTreeCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTreeCreate<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinaryTreeCreate<T> LeftTree { get; set; }
+        public BinaryTreeCreate<T> RightTree { get; set; }
+        public BinaryTreeCreate(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T CurrentNodeValue = this.NodeData;
+            if ((CurrentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinaryTreeCreate<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinaryTreeCreate<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BinarySearchTree;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" ***** Welcome to Binary Search Tree ***** ");
+        BinaryTreeCreate<int> tree = new BinaryTreeCreate<int>(56);
+        tree.Insert(30);
+        tree.Insert(70);
+        tree.Display();
+    }
+}
